240d3d3ab612312227f1489c1949742cb9428731
Figo <wf851128@gmail.com>
2021-12-13 15:39:15 +0800

    headBar 完成改写

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,25 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view/>
+  <HeadBar />
+  <router-view />
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import HeadBar from '../src/components/HeadBar.vue'
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+export default {
+  components: {
+    HeadBar
   }
+}
+</script>
+
+<style lang="scss" scoped>
+@import url("./assets/css/iconfont.css");
+html,
+body {
+  width: 100%;
+  height: 100%;
+  font-family: IBMPlexSans, Arial, sans-serif;
+  background: #eeeeee;
 }
 </style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,39 @@
 import { createStore } from 'vuex'
 
+const defaultLoginResult = {
+  token: null,
+  user_id: null,
+  user_name: null
+}
 export default createStore({
   state: {
+    isLogin: false,
+    loginResult: defaultLoginResult
   },
   mutations: {
+    init (state) {
+      const loginResult = JSON.parse(localStorage.getItem('loginResult'))
+      console.log(localStorage.getItem('loginResult'))
+      if (loginResult != null) {
+        state.loginResult = loginResult
+      }
+    },
+    login (state, loginResult) {
+      state.loginResult = loginResult
+    },
+    logout (state) {
+      localStorage.removeItem('loginResult')
+      state.loginResult = defaultLoginResult
+    }
   },
   actions: {
   },
   modules: {
+  },
+  getters: {
+    isLogin: state => state.loginResult.user_id !== null,
+    userID: state => state.loginResult.user_id,
+    username: state => state.loginResult.user_name,
+    accessToken: state => state.loginResult.token
   }
 })
